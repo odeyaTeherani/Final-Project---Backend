3a8307be450c3dbb1ad9ceffbe76e62ca6432fb5
davidbogomolov <davidbogomolov@gmail.com>
2020-04-28 18:18:50 +0300

    fix broken  master

--- backend/Business/Services/EventTypeService.cs
+++ backend/Business/Services/EventTypeService.cs
@@ -33,7 +33,7 @@ namespace backend.Business.Services
 
         public async Task<EventTypeDto> GetByIdAsync(int id)
         {
-            var result = await _context.EventTypes.SingleOrDefaultAsync(e => e.Id == id)
+            var result = await _context.EventTypes.SingleOrDefaultAsync(e => e.Id == id);
             if (result == null) throw new CustomException($"EventType whit id {id} not found", HttpStatusCode.NotFound);
             return _mapper.Map<EventTypeDto>(result);
         }
