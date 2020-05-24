240072761e2df59c94ca60c92b31e14340cc9e09
Odeya Teherani <odeyalosh123@gmail.com>
2020-05-24 13:04:58 +0300

    jasonProperty in EventDto.cs

--- backend/Business/Dto/EventDto.cs
+++ backend/Business/Dto/EventDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using backend.Data.Enums;
 using backend.Data.Models;
+using Newtonsoft.Json;
 
 namespace backend.Business.Dto
 {
@@ -10,19 +11,44 @@ namespace backend.Business.Dto
         public int Id { get; set; } //primary key
         public int EventTypeId { get; set; }
         public EventType EventType { get; set; }
+        
         public Location Location { get; set; }
+        [JsonProperty("severityLevel")]
         public SeverityLevel SeverityLevelType { get; set; }
+        
+        [JsonProperty("createDate")]
         public DateTime CreateDate { get; set; }
+        
+        [JsonProperty("endDate")]
         public DateTime EndDate { get; set; }
+        
+        [JsonProperty("startDate")]
         public DateTime StartDate { get; set; }
+        
+        [JsonProperty("numOfInjured")]
         public int NumOfInjured { get; set; }
+        
+        [JsonProperty("numOfDead")]
         public int NumOfDead { get; set; }
+        
+        [JsonProperty("numOfPolice")]
         public int NumOfPolice { get; set; }
+        
+        [JsonProperty("numOfAmbulances")]
         public int NumOfAmbulances { get; set; }
+        
+        [JsonProperty("numOfFirefighters")]
         public int NumOfFirefighters { get; set; }
+        
+        [JsonProperty("numOfEnvironment")]
         public int NumOfEnvironment { get; set; }
+        
+        [JsonProperty("numOfZakaCars")]
         public int NumOfZakaCars { get; set; }
+        
+        [JsonProperty("nameInCharge")]
         public string NameInCharge { get; set; }
+        
         public List<Report> Reports { get; set; }
         public List<string> Images { get; set; }
     }
