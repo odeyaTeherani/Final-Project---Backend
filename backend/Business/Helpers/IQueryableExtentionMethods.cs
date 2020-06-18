329e243ce89571544589ce489984bfd224b9691b
davidbogomolv <davidbogomolov@gmail.com>
2020-06-18 11:38:16 +0300

    filter users by email, name, subRole

--- backend/Business/Helpers/IQueryableExtentionMethods.cs
+++ backend/Business/Helpers/IQueryableExtentionMethods.cs
@@ -7,7 +7,7 @@ namespace backend.Business.Helpers
 {
     public static class QueryableExtensionMethods
     {
-        public static IQueryable<Event> Filter(this IQueryable<Event> queryable, DateTime? date = null, int? eventTypeId = null, SeverityLevel? severityLevel = null)
+        public static IQueryable<Event> FilterEvents(this IQueryable<Event> queryable, DateTime? date = null, int? eventTypeId = null, SeverityLevel? severityLevel = null)
         {
             if (date.HasValue)
             {
@@ -26,6 +26,29 @@ namespace backend.Business.Helpers
             {
                 queryable = queryable.Where(x =>
                     x.SeverityLevelType.HasValue && x.SeverityLevelType.Value == severityLevel.Value);
+            }
+
+            return queryable;
+        }
+        
+        public static IQueryable<ApplicationUser> FilterUsers(this IQueryable<ApplicationUser> queryable, string name = null, string email = null, int? subRoleId = null)
+        {
+            if (name != null)
+            {
+                queryable = queryable.Where(x =>
+                    x.FirstName == name || x.LastName == name);
+            }
+            
+            if (email != null)
+            {
+                queryable = queryable.Where(x =>
+                    x.Email == email);
+            }
+            
+            if (subRoleId.HasValue)
+            {
+                queryable = queryable.Where(x =>
+                    x.SubRoleId == subRoleId);
             }
 
             return queryable;
