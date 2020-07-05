72e13bba77fd983da675aad11aafef86378bdd5c
odeya teherani <davidbogomolov@gmail.com>
2020-07-05 19:13:38 +0300

    create ConsultDto.cs, Create method 'GetConsultAsync' in EventService.cs

--- backend/Business/Services/EventService.cs
+++ backend/Business/Services/EventService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AutoMapper;
 using backend.Business.Dto;
@@ -11,7 +12,6 @@ using backend.Controllers;
 using backend.Data;
 using backend.Data.Enums;
 using backend.Data.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Business.Services
@@ -20,12 +20,14 @@ namespace backend.Business.Services
     {
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IHttpClientFactory _httpClientFactory;
         private readonly QueryHelper<Event> _queryHelper;
 
-        public EventService(ApplicationDbContext context, IMapper mapper)
+        public EventService(ApplicationDbContext context, IMapper mapper, IHttpClientFactory httpClientFactory)
         {
             _context = context;
             _mapper = mapper;
+            _httpClientFactory = httpClientFactory;
             _queryHelper = new QueryHelper<Event>(_context);
         }
 
@@ -33,7 +35,8 @@ namespace backend.Business.Services
         /// Get all events
         /// </summary>
         /// <returns>List of all events</returns>
-        public async Task<List<EventDto>> GetAllAsync(DateTime? date = null,int? eventTypeId= null,SeverityLevel? severityLevel = null)
+        public async Task<List<EventDto>> GetAllAsync(DateTime? date = null, int? eventTypeId = null,
+            SeverityLevel? severityLevel = null)
         {
             if (ShouldFilter(date, eventTypeId, severityLevel))
             {
@@ -42,14 +45,36 @@ namespace backend.Business.Services
                     .ToListAsync();
 
                 var orderByDescending = all.OrderByDescending(e => e.StartDate);
-                return _mapper.Map<List<EventDto>>(orderByDescending);   
+                return _mapper.Map<List<EventDto>>(orderByDescending);
             }
             else
             {
                 var all = await GetEvent()
                     .ToListAsync();
                 var orderByDescending = all.OrderByDescending(e => e.StartDate);
-                return _mapper.Map<List<EventDto>>(orderByDescending);   
+                return _mapper.Map<List<EventDto>>(orderByDescending);
+            }
+        }
+
+        public async Task<ConsultDto> GetConsultAsync(int? hours = null, int? eventTypeId = null,
+            SeverityLevel? severityLevel = null)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "http://127.0.0.1:7000/?event_type=" + eventTypeId + "&severity_level=" + (int) severityLevel +
+                "&date_diff=" + hours);
+
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsAsync<ConsultDto>();
+                return data;
+            }
+            else
+            {
+                throw new CustomException("connect to server failed");
             }
         }
 
@@ -71,7 +96,7 @@ namespace backend.Business.Services
 
             var originalReports = _context.Reports.Where(report => reportsIds.Contains(report.Id)).ToList();
             originalReports.ForEach(report => { report.Event = mappedEvent; });
-            
+
             await _context.SaveChangesAsync();
             return _mapper.Map<EventDto>(mappedEvent);
         }
@@ -80,7 +105,7 @@ namespace backend.Business.Services
         {
             var result = await _context.Events.SingleOrDefaultAsync(e => e.Id == id);
             if (result == null) throw new CustomException($"Event whit id {id} not found", HttpStatusCode.NotFound);
-            
+
             var mappedEvent = _mapper.Map<Event>(updateEvent);
             result.StartDate = updateEvent.StartDate;
             result.EndDate = updateEvent.EndDate;
@@ -96,7 +121,7 @@ namespace backend.Business.Services
             result.Note = mappedEvent.Note;
             result.EventTypeId = updateEvent.EventType.Id;
             // result.Images. = mappedEvent.Images.;
-           // result.Location = updateEvent.Location;
+            // result.Location = updateEvent.Location;
             // result.Reports = updateEvent.Reports;
             result.SeverityLevelType = updateEvent.SeverityLevelType;
             await _context.SaveChangesAsync();
@@ -104,20 +129,21 @@ namespace backend.Business.Services
 
         public async Task DeleteAsync(int id)
         {
-            var result = await _context.Events.SingleOrDefaultAsync(e => e.Id == id); // Make sure it is single and if you didn't find return null
+            var result =
+                await _context.Events.SingleOrDefaultAsync(e =>
+                    e.Id == id); // Make sure it is single and if you didn't find return null
             if (result == null) throw new CustomException($"Event whit id {id} not found", HttpStatusCode.NotFound);
             _context.Events.Remove(result);
             await _context.SaveChangesAsync();
         }
-        
+
         private IQueryable<Event> GetEvent()
         {
             return _queryHelper.GetAllIncluding(x => x.EventType, x => x.Images, x => x.Location);
         }
-        
-     
 
-        private bool ShouldFilter(DateTime? date = null,int? eventTypeId= null,SeverityLevel? severityLevel = null)
+
+        private bool ShouldFilter(DateTime? date = null, int? eventTypeId = null, SeverityLevel? severityLevel = null)
         {
             return date.HasValue || eventTypeId.HasValue || severityLevel.HasValue;
         }
